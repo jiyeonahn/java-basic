7c50ee60a7663f089473a21bdf43b29ddc05bdac
jiyeonahn <ajy7424@naver.com>
2025-02-04 19:50:27 +0900

    상속2

--- src/extends1/overriding/ElectricCar.java
+++ src/extends1/overriding/ElectricCar.java
@@ -7,7 +7,7 @@ public class ElectricCar extends Car {
         System.out.println("전기차를 빠르게 움직입니다.");
     }
 
-    public void charge(){
+    public void charge(int b, int a){
         System.out.println("충전합니다.");
     }
 }
